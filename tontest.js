85cd83770578d14e902208e75d82b42611a25cbe
Annniiiii <andrea.rauh@haw-hamburg.de>
2020-12-12 16:10:17 +0100

    Ton Farbe Kombination (tontest.js)

--- tontest.js
+++ tontest.js
@@ -1,4 +1,54 @@
-//plieseignore:)
-var element = document.getElementById('image_1'),
+
+let context = new AudioContext();
+
+var element = document.getElementById('B1_2'),
     style = window.getComputedStyle(element),
-    top = style.getPropertyValue('top');
+    color = style.getPropertyValue('background-color');
+    document.write (color);
+
+if (color === "rgb(255, 0, 0)") {
+    document.querySelector("#Startbutton").addEventListener("mouseup", function(e){
+        let sound = new Audio("/Sounds/sound1.wav");
+        let soundNode = context.createMediaElementSource(sound);
+        let gainNode = context.createGain();    
+        gainNode.gain.value = 0.8;    
+        soundNode.connect(gainNode);
+        gainNode.connect(context.destination);   
+        sound.play();
+    });    
+    }
+    else if (color === "rgb(0, 128, 0)") {
+        document.querySelector("#Startbutton").addEventListener("mouseup", function(e){
+            let sound = new Audio("/Sounds/sound2.wav");
+            let soundNode = context.createMediaElementSource(sound);
+            let gainNode = context.createGain();    
+            gainNode.gain.value = 0.8;    
+            soundNode.connect(gainNode);
+            gainNode.connect(context.destination);   
+            sound.play();
+        });
+        }
+        else if (color === "rgb(0, 0, 255)") {
+            document.querySelector("#Startbutton").addEventListener("mouseup", function(e){
+                let sound = new Audio("/Sounds/sound3.wav");
+                let soundNode = context.createMediaElementSource(sound);
+                let gainNode = context.createGain();   
+                gainNode.gain.value = 0.8;    
+                soundNode.connect(gainNode);
+                gainNode.connect(context.destination);  
+                sound.play();
+            });
+            }
+            else if (color === "rgb(255, 255, 0)") {
+                document.querySelector("#Startbutton").addEventListener("mouseup", function(e){
+                    let sound = new Audio("/Sounds/sound4.wav");
+                    let soundNode = context.createMediaElementSource(sound);
+                    let gainNode = context.createGain();   
+                    gainNode.gain.value = 0.8;   
+                    soundNode.connect(gainNode);
+                    gainNode.connect(context.destination);     
+                    sound.play();
+                });
+                }
+
+
