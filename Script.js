b06cd5d844f3b29ff42304d9cde365f73617fac5
kiamomo <37752277+kiamomo@users.noreply.github.com>
2020-12-12 16:19:42 +0100

    Aufg. 11 aus Session 3 eingebaut und schaue weiter

--- Script.js
+++ Script.js
@@ -1,17 +1,11 @@
-var context = new AudioContext();
-
-button = document.querySelector('#startButton');
+let context = new AudioContext();
 isPlaying = false;
-
-//Tempo initialisieren
-var tempo = 90; 
-var eighthNoteTime = (60 / tempo) / 2;
 
 // leeres Array für Sounds
 var audioBuffers = [];
 
 // Array für Felderkennung, 32 Felder wobei 0 aus ist und 1-4 verschiedene Farben
-var felder = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let felder = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // MIDI Initialisieren
 if (navigator.requestMIDIAccess) {
@@ -39,27 +33,63 @@ function onMIDIMessage(event) {
 }
 
 // Audiosound laden
-for (let i = 0; i < 4; i++)
+for (let i = 0; i < 3; i++)
     getAudioData(i);
 
 function getAudioData(i) {
-    fetch("Sound/sound" + (i + 1) + ".wav")
-    .then(response => response.arrayBuffer())
-    .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
-    .then(audioBuffer => {
-        audioBuffers[i] = audioBuffer;
+    fetch("/sounds/sound" + (i + 1) + ".wav")
+        .then(response => response.arrayBuffer())
+        .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
+        .then(audioBuffer => {
+            audioBuffers[i] = audioBuffer;
     })
     .catch(console.error);
 }
 
 // Funktion um einen Sound zu bestimmten Zeitpunkt abzuspielen
 function playSound(buffer, time) {
-    var source = context.createBufferSource();
+    let source = context.createBufferSource();
     source.buffer = buffer;
     source.connect(context.destination);
     source.start(time);
 }
 
+function playBeat() {
+    let tempo = 90; // BPM (beats per minute)
+    let eighthNoteTime = (60 / tempo) / 2;
+    let startTime = context.currentTime;
+    let bassdrum = audioBuffers[0];
+    let snaredrum = audioBuffers[1];
+    let hihat = audioBuffers[2];
+
+    for (let takt = 0; takt < 2; takt++) {
+        let time = startTime + (takt * 8 * eighthNoteTime);
+
+        playSound(bassdrum, time + 0 * eighthNoteTime);
+        playSound(bassdrum, time + 1 * eighthNoteTime);
+        playSound(bassdrum, time + 4 * eighthNoteTime);
+
+        playSound(snaredrum, time + 2 * eighthNoteTime);
+        playSound(snaredrum, time + 3.5 * eighthNoteTime);
+        playSound(snaredrum, time + 4.5 * eighthNoteTime);
+        playSound(snaredrum, time + 6 * eighthNoteTime);
+
+        for (let j = 0; j < 8; j++) {
+            playSound(hihat, time + j * eighthNoteTime);
+        }
+    }
+}
+
+/*
+if (isPlaying) {
+    playBeat();
+}
+*/
+document.querySelector("#startButton").addEventListener("click", function(e) {
+    playBeat();
+});
+
+/*
 startButton.addEventListener("click", function (e) {
     if (isPlaying) {
         startButton.innerHTML = "Start";
@@ -68,3 +98,5 @@ startButton.addEventListener("click", function (e) {
     }
     isPlaying = !isPlaying;
 });
+
+*/
