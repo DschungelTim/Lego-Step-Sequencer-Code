683dfd215019261cf0e109d7091b30caa9277aee
kiamomo <37752277+kiamomo@users.noreply.github.com>
2020-12-12 12:33:48 +0100

    Arbeit zum Verstehen von midi.py

--- midi.py
+++ midi.py
@@ -4,13 +4,13 @@ import cv2
 
 # Midi Ports suchen
 print("MIDI output ports: ", mido.get_output_names())
-midiOutput = mido.open_output("Microsoft GS Wavetable Synth 0")
+midiOutput = mido.open_output("LoopBe Internal MIDI 1")
 
-def sendControlchange(control, value):
-	message = mido.Message('control_change', control=control, value=value)
+def sendMidiNote(note, velocity):
+	message = mido.Message('note_on', note=note, velocity=velocity)
 	midiOutput.send(message)
 
 
 for value in range(128):
-	sendControlchange(7, value)
+	sendMidiNote(7, value)
 	cv2.waitKey(500)
