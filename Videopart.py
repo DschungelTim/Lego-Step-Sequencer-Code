d66d1d3557f77337127cec8d1033dff2c8747315
DschungelTim <tim.passgang@haw-hamburg.de>
2021-01-25 17:26:45 +0100

    Kommentar Videopart

--- Videopart.py
+++ Videopart.py
@@ -4,11 +4,12 @@ import rtmidi
 import mido
 import time
 
-# Midi Port
+# Midi Port initialisieren.
 midiOutput = mido.open_output("LoopBe Internal MIDI 1")
 
+# Definieren der Farbwerte der Legosteine, durch Testen ermittelt
 # Wertebereich Hue: 0-180, Sat: 0-255
-# [Hue_min, Hue_max, Sat_min, Sat_max]
+# Datenstruktur: [Hue_min, Hue_max, Sat_min, Sat_max]
 gruen = [60, 90, 153, 255]
 hellblau = [80, 105, 230, 255]
 rot = [160, 190, 190, 245]
@@ -16,16 +17,16 @@ orange = [0, 15, 130, 210]
 ecke = [100, 120, 204, 255]
 blau = [100, 120, 204, 255]
 
-# Koordinateneckpunkte initialisieren
+# Koordinateneckpunkte initialisieren.
 x0 = 0
 x1 = 0
 y0 = 0
 y1 = 0
 
-# Array für Senden der Daten über Midi
+# Array für Senden der Daten über Midi.
 senden = []
 
-# dict für javascriptfelder
+# Übersetzungstabelle für das Senden der Felder über Midi.
 felderDict = {
     (1, 1) : 0,
     (2, 1) : 1,
@@ -68,10 +69,10 @@ kernelClosing = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
 # Kernel für Opening Filter
 kernelOpening = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
 
-#kernel für Erosion Filter
+# Kernel für Erosion Filter
 kernelErosion = np.ones((7,7), np.uint8)
 
-# Funktion, um Masken zu erstellen und durch Filter zu verbessern
+# Funktion, um Masken zu erstellen und durch Filter zu verbessern.
 def Masken(hue_min, hue_max, sat_min, sat_max) :
     mask_h = cv2.inRange(h, hue_min, hue_max)
     mask_s = cv2.inRange(s, sat_min, sat_max)
@@ -82,7 +83,7 @@ def Masken(hue_min, hue_max, sat_min, sat_max) :
     mask = cv2.erode(mask, kernelErosion, iterations=2)
     return mask
 
-# Funktion, um die Schwerpunkte der einzelnen Steine zu berechnen
+# Funktion, um die Schwerpunkte der einzelnen, erkannten Steine zu berechnen.
 def Schwerpunkte(maske) :
     contours, _ = cv2.findContours(maske, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
     centres = []
@@ -94,31 +95,31 @@ def Schwerpunkte(maske) :
             print("Wups")
     return centres
 
-# Funktion, um die erkannten x- und y-Koordinaten in unser definiertes Raster einzuordnen.
+# Funktion, um die erkannten X- und Y-Koordinaten der ermittelten Schwerpunkte der Steine in unser definiertes Raster einzuordnen.
 def PinKoord (SPunktarray) :
-    dx = x1 - x0
-    dy = y1 - y0
+    dx = x1 - x0    # Länge unserer X-Achse
+    dy = y1 - y0    # Länge unserer Y-Achse
     yxFelder = [[], []]
     Wert = 0
-    for i in range(0, len(SPunktarray)) :
+    for i in range(0, len(SPunktarray)) :   # X- und Y-Koordinate einlesen.
         X = SPunktarray [i] [0]
         Y = SPunktarray [i] [1]
         for i in range (0, 10) :
-            if (x0 + (i/10) * dx) < X <= (x1 - ((9-i)/10) * dx) :
-                if i == 8 or i == 9:
+            if (x0 + (i/10) * dx) < X <= (x1 - ((9-i)/10) * dx) :               # Schauen, in welchem Feld auf der X-Achse der Stein liegt.   
+                if i == 8 or i == 9:                                            # Schauen, ob der erkannte Stein im Bereich der Slider liegt.
                     Wert = ((Y-127)/192)*127
                     Wert = round(Wert)   
-                    sendMidiNote(i+24, 127-Wert) #i+40, damit Note gesendet wird, die sehr unterschiedlich zu "normalen Feldern" ist
-                if 0 <= i <= 7:
+                    sendMidiNote(i+24, 127-Wert)                                # Senden der Position des Steines, um Sliderwerte zu aktualisieren.
+                if 0 <= i <= 7:                                                 
                     xFelder = yxFelder[1]
                     xFelder.append(i+1)   
                     for i in range (0, 4) :
-                        if (y0 + (i/4) * dy) < Y <= (y1 -((3 - i)/4) * dy) :
+                        if (y0 + (i/4) * dy) < Y <= (y1 -((3 - i)/4) * dy) :    # Schauen, in welchem Feld auf der Y-Achse der Stein liegt.
                             yFelder = yxFelder[0]
                             yFelder.append(i+1)
     return yxFelder
 
-# Das senden Array wird zurückgesetzt und somit genullt.
+# Das senden-Array wird zurückgesetzt und somit genullt.
 # Danach wird das Array mit den zu sendenden Velocities an den zu sendenden Noten befüllt.
 # Somit werden nur die Felder farbig, welche auch tatsächlich erkannt werden.
 def sendMIDIContainer():
@@ -144,7 +145,7 @@ def sendMidiNote(note, velocity):
     print(message)
 
 # Schaut welches Array im Argument steht und sendet dann die dementsprechende Midi Note.
-# Über das Wörterbuch werden die hier errechneten Werte in die vom Javascript erwarteten umgerechnet.
+# Über die Übersetzungstabelle werden die hier errechneten Werte in die vom Javascript erwarteten umgerechnet.
 # Die Zahl 1-5 gibt die Farbe des Steins an.
 # Dem Array werden an den dementsprechenden Stellen dementsprechende Velocity werte eingesetzt.
 def sendMIDI (felder):
@@ -165,21 +166,23 @@ def sendMIDI (felder):
         
         if felder == felder_blau:
             senden[felderDict[(felder[0][i], felder[1][i])]] = 5
-                
 
+
+# Counter, um nur bei dem allerersten Frame die Ecksteine zu erkennen und damit das Koordinatensystem zu initialisieren.
 counter = 0
+
 # Webcam öffnen
 cap = cv2.VideoCapture(0)
 
-# Hauptschleife während Kamera geöffnet ist
+# Hauptschleife, während Kamera geöffnet ist.
 while cap.isOpened():
     ret, frame = cap.read()
 
-    # Bild in HSV splitten
+    # Bild in HSV splitten.
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(hsv)
 
-        # Eckpunkte des Koordinatensystems berechnen
+    # Eckpunkte des Koordinatensystems erkennen und festlegen.
     try:
         if counter == 0:
             mask_ecke = Masken(ecke[0], ecke[1], ecke[2], ecke[3])
@@ -196,21 +199,21 @@ while cap.isOpened():
                 y1 = center_ecke[0] [1]
         counter = counter + 1
 
-        # Maskenfunktion für jede Farbe aufrufen
+        # Maskenfunktion für jede Farbe aufrufen.
         mask_hellblau = Masken(hellblau[0], hellblau[1], hellblau[2], hellblau[3])
         mask_rot = Masken(rot[0], rot[1], rot[2], rot[3])
         mask_gruen = Masken(gruen[0], gruen[1], gruen[2], gruen[3])
         mask_orange = Masken(orange[0], orange[1], orange[2], orange[3]) 
         mask_blau = Masken(blau[0], blau[1], blau[2], blau[3])
 
-        # Funktionen aufrufen, um Schwerpunkte zu berechen, Ergebis wird als Array gespeichert mit jeweils x,y Koordinate
+        # Funktionen aufrufen, um Schwerpunkte zu berechen, Ergebnis wird als Array gespeichert mit jeweils x- und y-Koordinaten.
         center_hellblau = Schwerpunkte(mask_hellblau)
         center_rot = Schwerpunkte(mask_rot)
         center_gruen = Schwerpunkte(mask_gruen)
         center_orange = Schwerpunkte(mask_orange)
         center_blau = Schwerpunkte(mask_blau)
     
-        # Funktion aufrufen, um die center_daten in 2D Array mit informationen über "aktivierte" Felder umzuwandeln
+        # Funktion aufrufen, um die center_daten in 2D Array mit Informationen über "aktivierte" Felder umzuwandeln.
         # Für drei rote Steine könnte dann zB. ein solches Array mit y und x Felderzahlen herauskommen:
         # felder_rot = [[1, 4], [1, 8]] für zwei Steine ganz oben links und ganz unten rechts.
         # felder =[[die y-felder zahlen von 1-4], [die x-felder zahlen von 1-8]]
@@ -219,12 +222,16 @@ while cap.isOpened():
         felder_gruen = PinKoord (center_gruen)
         felder_orange = PinKoord (center_orange)
         felder_blau = PinKoord (center_blau)
-        # Hierüber werden alle Midi Sendungen geregelt
+
+        # Hierüber werden alle Midi Sendungen geregelt.
         sendMIDIContainer()
+
+        # Kurzes Delay, damit der Midiport nicht überlastet wird.
         time.sleep(0.1)
     except:
         pass
-    # Video und Masken anzeigen
+
+    # Video und Masken anzeigen.
     cv2.imshow("Video", frame)
     cv2.imshow("Rot", mask_rot)
     cv2.imshow("Hellblau", mask_hellblau)
